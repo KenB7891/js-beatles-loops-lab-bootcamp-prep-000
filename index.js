01c8f6f348043198ad465b36cbcfb27436296e54
Kenneth Bilicke <kbilicke7891@gmail.com>
2018-03-10 00:26:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function theBeatlesPlay(array1,array2) {
+  var myArray = []
+  
+  for (var i= 0; i = array1.length - 1; i++) {
+    myArray[i] = `${array1[i]} plays ${array2[i]}`
+  }
+  
+  return myArray
+  
+}
